3a5a448620776659ccf103c4e5499c0af00d5971
Ling Fung Chan <justinlfchan@gmail.com>
2022-11-09 00:47:38 +0000

    added logic for the 10th frame

--- bowling_calculator.js
+++ bowling_calculator.js
@@ -8,18 +8,29 @@ const bowlingScore = (rolls) => {
   while (i < rolls.length - 1 && frame < 10) {
     // If it is a strike, there will be only 1 roll in that frame
     if (rolls[i] === 10) {
-      total += rolls[i] + rolls[i + 1] + rolls[i + 2];
+      score += rolls[i] + rolls[i + 1] + rolls[i + 2];
       i += 1;
       // If it is a spare and not-strike or not-spare, there will be 2 rolls in that frame
     } else if (rolls[i] + rolls [i + 1] === 10) {
-      total += 10 + rolls[i + 2];
+      score += 10 + rolls[i + 2];
       i += 2;
     } else {
-      total = rolls[i] + rolls[i + 1];
+      score = rolls[i] + rolls[i + 1];
       i += 2;
     }
     frame += 1;
   }
+  // When it comes to the 10th frame
+  // In case of spare or strike, no more bonus is added
+  // if the last frame is a spare, player rolls 1 bonus ball (3 rolls total)
+  // if the last frame is a strike, player rolls 2 bonus balls (3 rolls total)
+  // if neither a spare, nor a strike is scored, the player does not get the 3rd roll
+  if (rolls[i] === 10 || rolls[i] + rolls [i + 1] === 10) {
+    score += rolls[i] + rolls[i + 1] + rolls[i + 2];
+  } else {
+    score += rolls[i] + rolls[i + 1];
+  }
+  return score;
 };
 
 // rolls is an array storing every single score from frame 1 to 10
