ed6f94b528b9155eeb00903ddb660fec912120cf
Ling Fung Chan <justinlfchan@gmail.com>
2022-11-09 01:00:49 +0000

    fixed bug for calculating score of non-strike and non-spare case by replace '-' with  +=

--- bowling_calculator.js
+++ bowling_calculator.js
@@ -11,21 +11,21 @@ const bowlingScore = (rolls) => {
       score += rolls[i] + rolls[i + 1] + rolls[i + 2];
       i += 1;
       // If it is a spare and not-strike or not-spare, there will be 2 rolls in that frame
-    } else if (rolls[i] + rolls [i + 1] === 10) {
+    } else if (rolls[i] + rolls[i + 1] === 10) {
       score += 10 + rolls[i + 2];
       i += 2;
     } else {
-      score = rolls[i] + rolls[i + 1];
+      score += rolls[i] + rolls[i + 1];
       i += 2;
     }
     frame += 1;
-  }
+  };
   // When it comes to the 10th frame
   // In case of spare or strike, no more bonus is added
   // if the last frame is a spare, player rolls 1 bonus ball (3 rolls total)
   // if the last frame is a strike, player rolls 2 bonus balls (3 rolls total)
   // if neither a spare, nor a strike is scored, the player does not get the 3rd roll
-  if (rolls[i] === 10 || rolls[i] + rolls [i + 1] === 10) {
+  if (rolls[i] === 10 || rolls[i] + rolls[i + 1] === 10) {
     score += rolls[i] + rolls[i + 1] + rolls[i + 2];
   } else {
     score += rolls[i] + rolls[i + 1];
